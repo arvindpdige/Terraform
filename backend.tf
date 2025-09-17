5562d29f26cce20337e51d66c7ade8cdd6021b7d
arvindpdige <arvindpdige@gmail.com>
2025-09-17 12:20:49 +0530

    Added rg in backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "azurerm" {
     # use_msi              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
-    # use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    # use_azuread_auth     = true    
+    resource_group_name  = "backend_tf_rg"                                # Can also be set via `ARM_USE_AZUREAD` environment variable.
     storage_account_name = "mystoragefortf"                       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"                 #blob name where state file is written  # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.      
