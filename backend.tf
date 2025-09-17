62828ec1151c2ade4deed0a6808a34098eb1f210
arvindpdige <arvindpdige@gmail.com>
2025-09-17 12:11:55 +0530

    check now

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    use_msi              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
+    # use_msi              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
     use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
     storage_account_name = "mystoragefortf"                       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
